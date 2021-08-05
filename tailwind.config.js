4bfef33863d28b54b89de382137f2ddb5ed5661e
Mariusz Rakus <mariusz@raqz.pl>
2021-08-05 20:30:50 +0200

    feat: Basic environment setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: '#d2e3ff',
+          100: '#b4d0ff',
+          200: '#a5c7ff',
+          300: '#87b4ff',
+          400: '#78abff',
+          500: '#69a2ff',
+          600: '#5e91e5',
+          700: '#5481cc',
+          900: '#0a1019'
+        }
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
---
+++ views/DetailView/DetailView.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const DetailView = () => <div>Detail View</div>;
